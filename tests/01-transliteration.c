47a2f0f2cffa166a1f2e0954d9a7b8fd37d1d670
Navaneeth.K.N <navaneethkn@gmail.com>
2013-04-15 07:53:24 +0530

    Adding tests for basic transliteration

--- tests/01-transliteration.c
+++ tests/01-transliteration.c
@@ -23,64 +23,114 @@ actual characters in the supported languages */
 #include <string.h>
 #include "../varnam.h"
 
-/* static int rendering_vowels(varnam *handle) */
-/* { */
-/*     varray *words; */
+static int rendering_vowels(varnam *handle)
+{
+    vword* word;
+    varray *words = varray_init();
 
-/*     varnam_transliterate(handle, "oof", &output); */
-/*     printf("%s\n", output); */
-/*     if(strcmp(output, "v-oof~") == 0)  */
-/*         return 0; */
-/*     return 1; */
-/* } */
+    varnam_transliterate(handle, "a", &words);
 
-/* static int rendering_dependent_vowels(varnam *handle) */
-/* { */
-/*     char *output; */
+    if (varray_length(words) != 1) {
+        printf("Expected transliterate to return 1 word but it returned %d words\n", varray_length(words));
+        return 0;
+    }
 
-/*     varnam_transliterate(handle, "foo", &output); */
-/*     printf("%s\n", output); */
-/*     if(strcmp(output, "fdv-oo") == 0)  */
-/*         return 0; */
-/*     return 1; */
-/* } */
+    word = varray_get(words, 0);
+    if(strcmp(word->text, "a-value1") == 0) {
+        return 0;
+    }
 
-/* static void log_fun(const char* msg) */
-/* { */
-/*     printf("%s\n", msg); */
-/* } */
+    return 1;
+}
+
+static int rendering_dependent_vowels(varnam *handle)
+{
+    vword* word;
+    varray *words = varray_init();
+
+    varnam_transliterate(handle, "aaa", &words);
+
+    if (varray_length(words) != 1) {
+        printf("Expected transliterate to return 1 word but it returned %d words\n", varray_length(words));
+        return 0;
+    }
+
+    word = varray_get(words, 0);
+    if(strcmp(word->text, "aa-value1a-value2") == 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+static int rendering_cancelation_character(varnam *handle)
+{
+    vword* word;
+    varray *words = varray_init();
+
+    varnam_transliterate(handle, "aa_a", &words);
+
+    if (varray_length(words) != 1) {
+        printf("Expected transliterate to return 1 word but it returned %d words\n", varray_length(words));
+        return 0;
+    }
+
+    word = varray_get(words, 0);
+    if(strcmp(word->text, "aa-value1a-value1") == 0) {
+        return 0;
+    }
+
+    return 1;
+}
+
+static void log_fun(const char* msg)
+{
+    printf("%s\n", msg);
+}
 
 int basic_transliteration(int argc, char **argv)
 {
-    /* varnam *handle; */
-    /* int rc; */
-    /* char *msg; */
+    varnam *handle;
+    int rc;
+    char *msg;
 
-    /* if(argc == 0) { */
-    /*     printf("no scheme file specified\n"); */
-    /*     return 1; */
-    /* } */
-    /* printf("%s\n", argv[0]); */
-    /* rc = varnam_init(argv[0], &handle, &msg); */
-    /* if(rc != VARNAM_SUCCESS) { */
-    /*     printf("initialization failed - %s\n", msg); */
-    /*     return 1; */
-    /* } */
+    rc = varnam_init("output/01-t.vst", &handle, &msg);
+    if(rc != VARNAM_SUCCESS) {
+        printf("initialization failed - %s\n", msg);
+        return 1;
+    }
 
-    /* rc = varnam_enable_logging(handle, VARNAM_LOG_DEBUG, &log_fun); */
-    /* printf("Return code is %d", rc); */
+    rc = varnam_enable_logging(handle, VARNAM_LOG_DEBUG, &log_fun);
 
-    /* rc = rendering_vowels(handle); */
-    /* if(rc != VARNAM_SUCCESS) { */
-    /*     printf("rendering dependent vowels is not proper - \n"); */
-    /*     return 1; */
-    /* } */
+    rc = varnam_create_token (handle, "a", "a-value1", "a-value2", "", "", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0);
+    if(rc != VARNAM_SUCCESS) {
+        printf("Token creation failed - %s\n", varnam_get_last_error (handle));
+        return 1;
+    }
 
-    /* rc = rendering_dependent_vowels(handle); */
-    /* if(rc != VARNAM_SUCCESS) { */
-    /*     printf("rendering dependent vowels is not proper - \n"); */
-    /*     return 1; */
-    /* } */
+    rc = varnam_create_token (handle, "aa", "aa-value1", "aa-value2", "", "", VARNAM_TOKEN_VOWEL, VARNAM_MATCH_EXACT, 0, 0, 0);
+    if(rc != VARNAM_SUCCESS) {
+        printf("Token creation failed - %s\n", varnam_get_last_error (handle));
+        return 1;
+    }
+
+    rc = varnam_create_token (handle, "_", "", "", "", "", VARNAM_TOKEN_NON_JOINER, VARNAM_MATCH_EXACT, 0, 0, 0);
+    if(rc != VARNAM_SUCCESS) {
+        printf("Token creation failed - %s\n", varnam_get_last_error (handle));
+        return 1;
+    }
+
+    rc = rendering_vowels(handle);
+    if(rc != VARNAM_SUCCESS) {
+        printf("rendering dependent vowels is not proper - \n");
+        return 1;
+    }
+
+    rc = rendering_dependent_vowels(handle);
+    if(rc != VARNAM_SUCCESS) {
+        printf("rendering dependent vowels is not proper - \n");
+        return 1;
+    }
 
     return 0;
 }
