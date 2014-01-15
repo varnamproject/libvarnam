1cd8f0febc416cb35dd287273aa89201c2e56f43
navaneethkn <navaneethkn@gmail.com>
2014-01-15 22:52:53 +0530

    Creating a distro target which will create the distribution package

---
+++ makedistro.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+tmp_dir_create_command="mktemp -d"
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+	tmp_dir_create_command="mktemp -d -t libvarnam"
+fi
+
+target_dir=`$tmp_dir_create_command`
+target_version=$1
+target_dir="$target_dir/libvarnam-$target_version"
+mkdir $target_dir
+cp -r . $target_dir
+cd $target_dir
+git clean -f -x -d
+./varnamc --compile schemes/ml
+./varnamc --compile schemes/hi
+rm -rf .git
+cd ..
+tar -pczf "libvarnam-$target_version.tar.gz" "libvarnam-$target_version"
+gpg -b --use-agent "libvarnam-$target_version.tar.gz"
+gpg --verify "libvarnam-$target_version.tar.gz.sig"
+chmod 644 *
+echo "Created tarball: `pwd`/libvarnam-$target_version.tar.gz"
+
