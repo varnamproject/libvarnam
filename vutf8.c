4e1d8405a12e8467b421e9a2fa75dda0b4f4de60
Navaneeth K N <navaneethkn@gmail.com>
2014-03-24 07:46:25 +0530

    Fixing lintian warnings

--- vutf8.c
+++ vutf8.c
@@ -5,34 +5,7 @@
  * This is part of libvarnam. See LICENSE.txt for the license
  */
 
-
-/* 2009-02-13 */
-
-/*
-Copyright (c) 2005 JSON.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-The Software shall be used for Good, not Evil.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-/* This UTF8 decoder is ny JSON.org. I have modified this to add reentrancy support */
+/* This UTF8 decoder is by JSON.org. I have modified this to add reentrancy support */
 
 #include "vutf8.h"
 
