7b1dfd7618ffd39da7e03395fa4d3c1bf7f278e0
Navaneeth K N <navaneethkn@gmail.com>
2014-12-24 07:36:49 +0530

    Moving the tarball to source directory so that it can be archived

--- makedistro.sh
+++ makedistro.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+last_wd=`pwd`
 tmp_dir_create_command="mktemp -d"
 unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
@@ -16,6 +17,7 @@ git clean -f -x -d
 ./varnamc --compile schemes/ml
 ./varnamc --compile schemes/ml-inscript
 ./varnamc --compile schemes/hi
+./varnamc --compile schemes/pa
 rm -rf .git
 cd ..
 tar -pczf "libvarnam-$target_version.tar.gz" "libvarnam-$target_version"
@@ -23,4 +25,7 @@ tar -pczf "libvarnam-$target_version.tar.gz" "libvarnam-$target_version"
 #gpg --verify "libvarnam-$target_version.tar.gz.sig"
 chmod 644 *
 echo "Created tarball: `pwd`/libvarnam-$target_version.tar.gz"
-
+echo "Moving tarball to: $last_wd/distribution-tarball"
+rm -rf $last_wd/distribution-tarball
+mkdir -p $last_wd/distribution-tarball
+mv `pwd`/libvarnam-$target_version.tar.gz $last_wd/distribution-tarball
